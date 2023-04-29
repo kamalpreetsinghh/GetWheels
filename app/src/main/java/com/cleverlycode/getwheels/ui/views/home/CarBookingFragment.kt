acac717d0242eebc174beb1bdd023b8982814d31
Kamal Preet Singh <kamalpreetsingh025@gmail.com>
2023-04-28 23:51:04 -0400

    Stripe Payment Integration

--- app/src/main/java/com/cleverlycode/getwheels/ui/views/home/CarBookingFragment.kt
+++ app/src/main/java/com/cleverlycode/getwheels/ui/views/home/CarBookingFragment.kt
@@ -6,24 +6,53 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.cleverlycode.getwheels.GetWheelsAppState
 import com.cleverlycode.getwheels.databinding.FragmentCarBookingBinding
 import com.cleverlycode.getwheels.domain.models.BookingDetails
 import com.cleverlycode.getwheels.ui.viewmodels.CarBookingViewModel
 import com.cleverlycode.getwheels.utils.InternalStorageUtils
+import com.google.android.material.transition.MaterialContainerTransform
+import com.google.android.material.transition.MaterialFade
+import com.stripe.android.PaymentConfiguration
+import com.stripe.android.paymentsheet.PaymentSheet
+import com.stripe.android.paymentsheet.PaymentSheetResult
 import dagger.hilt.android.AndroidEntryPoint
+import org.json.JSONObject
 import java.time.LocalDate
 
 @AndroidEntryPoint
 class CarBookingFragment : Fragment() {
+    private val url = "https://getwheelsstripeapi.onrender.com/payment-sheet"
+    private var volleyResponse = MutableLiveData("")
+
+    private lateinit var paymentSheet: PaymentSheet
+    private lateinit var customerConfig: PaymentSheet.CustomerConfiguration
+    private lateinit var paymentIntentClientSecret: String
+
     private var _binding: FragmentCarBookingBinding? = null
     private val binding get() = _binding!!
 
     private val viewModel: CarBookingViewModel by viewModels()
 
     private val args: CarBookingFragmentArgs by navArgs()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        fetchStripeApiCredentials()
+        paymentSheet = PaymentSheet(this, ::onPaymentSheetResult)
+
+        enterTransition = MaterialFade()
+        reenterTransition = MaterialFade()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -45,6 +74,7 @@ class CarBookingFragment : Fragment() {
         binding.apply {
             checkoutButton.setOnClickListener {
                 viewModel.saveBookingDetails()
+                presentPaymentSheet()
             }
 
             buttonBack.setOnClickListener {
@@ -63,10 +93,67 @@ class CarBookingFragment : Fragment() {
                 )
             }
 
+            val nameObserver = Observer<String> { newName ->
+                // Update the UI, in this case, a TextView.
+                volley.text = newName
+            }
+
+            volleyResponse.observe(
+                viewLifecycleOwner,
+                nameObserver
+            )
+
             lifecycleOwner = this@CarBookingFragment
             viewmodel = viewModel
         }
 
         return binding.root
+    }
+
+    private fun fetchStripeApiCredentials() {
+        val queue = Volley.newRequestQueue(this.context)
+        val stringRequest = StringRequest(
+            Request.Method.POST,
+            url,
+            { response ->
+                val responseJson = JSONObject(response)
+                paymentIntentClientSecret = responseJson.getString("paymentIntent")
+                customerConfig = PaymentSheet.CustomerConfiguration(
+                    responseJson.getString("customer"),
+                    responseJson.getString("ephemeralKey")
+                )
+                val publishableKey = responseJson.getString("publishableKey")
+                PaymentConfiguration.init(this.requireContext(), publishableKey)
+            },
+            { val error = "That didn't work!" }
+        )
+
+        queue.add(stringRequest)
+    }
+
+    private fun presentPaymentSheet() {
+        paymentSheet.presentWithPaymentIntent(
+            paymentIntentClientSecret,
+            PaymentSheet.Configuration(
+                merchantDisplayName = "Get Wheels",
+                customer = customerConfig,
+                allowsDelayedPaymentMethods = false
+            )
+        )
+    }
+
+    private fun onPaymentSheetResult(paymentSheetResult: PaymentSheetResult) {
+        when(paymentSheetResult) {
+            is PaymentSheetResult.Canceled -> {
+                print("Canceled")
+            }
+            is PaymentSheetResult.Failed -> {
+                print("Error: ${paymentSheetResult.error}")
+            }
+            is PaymentSheetResult.Completed -> {
+                // Display for example, an order confirmation screen
+                print("Completed")
+            }
+        }
     }
 }
